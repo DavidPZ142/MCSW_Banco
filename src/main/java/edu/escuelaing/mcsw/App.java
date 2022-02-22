85911efea659e7abe707b81a5ed7bc0a6e43977e
DavidPZ666 <davidpz616@gmail.com>
2022-02-22 13:08:13 -0500

    First Commit

---
+++ src/main/java/edu/escuelaing/mcsw/App.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.mcsw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
